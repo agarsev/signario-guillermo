19b9ec0410a9cd4ab6052a5841f51b4880e33dcf
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2022-09-08 20:54:17 +0200

    Pagination

--- src/table/back.js
+++ src/table/back.js
@@ -4,11 +4,23 @@ const db = require('better-sqlite3')('../data/signario.db', {
     fileMustExist: true,
 });
 
-const selectQuery = db.prepare('SELECT * FROM signs LIMIT ?;');
+const PAGE_SIZE = 50;
+function numberOfPages() {
+    const res = db.prepare("SELECT COUNT(*) as count FROM signs;").get();
+    const count = parseInt(res.count);
+    return Math.floor(count / PAGE_SIZE)+((count % PAGE_SIZE)?1:0);
+}
+let numPages = numberOfPages();
 
 contextBridge.exposeInMainWorld('back', {
 
-    select: function () {
-        return selectQuery.all(10);
+    select: function (page) {
+        const order = 'number';
+        const asc = true;
+        const rows = db.prepare(`SELECT *
+            FROM signs ORDER BY ${order} ${asc?'ASC':'DESC'}
+            LIMIT ${PAGE_SIZE} OFFSET ${page*PAGE_SIZE}
+        ;`).all();
+        return { rows, numPages };
     }
 });
--- src/table/front.js
+++ src/table/front.js
@@ -4,22 +4,34 @@ import { useState, useEffect } from "react";
 function useTable () {
     const [ isLoading, setIsLoading ] = useState(true);
     const [ rows, setRows ] = useState([]);
+    const [ page, setPage ] = useState(0);
+    const [ numPages, setNumPages ] = useState(0);
     useEffect(() => {
-        const rows = back.select();
+        const { rows, numPages } = back.select(page);
         setRows(rows);
+        setNumPages(numPages);
         setIsLoading(false);
-    }, []);
-    return { isLoading, rows };
+    }, [page]);
+    return { isLoading, rows, page, numPages,
+        next: page<(numPages-1)?() => setPage(page+1):false,
+        prev: page>0?() => setPage(page-1):false,
+    };
 }
 
 function App () {
     const table = useTable();
     return <>
         <h1 className="text-primary-800">Guillermo</h1>
+        <nav>
+            Página:
+            <button onClick={table.prev?table.prev:undefined} disabled={!table.prev}>-</button>
+            {table.page+1}/{table.numPages}
+            <button onClick={table.next?table.next:undefined} disabled={!table.next}>+</button>
+        </nav>
         <table className="border-primary-200">
             <thead>
                 <tr>
-                    <th>Número</th>
+                    <th></th>
                     <th>Glosa</th>
                     <th>Signotación</th>
                     <th></th>
@@ -28,8 +40,8 @@ function App () {
             <tbody>
                 {table.isLoading?<tr>
                     <td colSpan="4">Cargando...</td>
-                </tr>:table.rows.map(r => <tr>
-                    <td>{r.number}</td>
+                </tr>:table.rows.map(r => <tr key={r.number}>
+                    <td className="text-sm text-gray-600">{r.number}</td>
                     <td>{r.gloss}</td>
                     <td>{r.notation}</td>
                     <td></td>
