689c63d81f46705bae1eee89eea76d0947e929ab
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2022-09-08 18:57:08 +0200

    React, Backend (electron preload), SQLITE

--- src/table/back.js
+++ src/table/back.js
@@ -1,1 +1,14 @@
-console.log("BACK");
+const { contextBridge } = require('electron');
+
+const db = require('better-sqlite3')('../data/signario.db', {
+    fileMustExist: true,
+});
+
+const selectQuery = db.prepare('SELECT * FROM signs LIMIT ?;');
+
+contextBridge.exposeInMainWorld('back', {
+
+    select: function () {
+        return selectQuery.all(10);
+    }
+});
--- src/table/front.js
+++ src/table/front.js
@@ -1,1 +1,42 @@
-console.log("FRONT");
+import { createRoot } from "react-dom/client";
+import { useState, useEffect } from "react";
+
+function useTable () {
+    const [ isLoading, setIsLoading ] = useState(true);
+    const [ rows, setRows ] = useState([]);
+    useEffect(() => {
+        const rows = back.select();
+        setRows(rows);
+        setIsLoading(false);
+    }, []);
+    return { isLoading, rows };
+}
+
+function App () {
+    const table = useTable();
+    return <>
+        <h1 className="text-primary-800">Guillermo</h1>
+        <table className="border-primary-200">
+            <thead>
+                <tr>
+                    <th>Número</th>
+                    <th>Glosa</th>
+                    <th>Signotación</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {table.isLoading?<tr>
+                    <td colSpan="4">Cargando...</td>
+                </tr>:table.rows.map(r => <tr>
+                    <td>{r.number}</td>
+                    <td>{r.gloss}</td>
+                    <td>{r.notation}</td>
+                    <td></td>
+                </tr>)}
+            </tbody>
+        </table>
+    </>;
+}
+
+createRoot(document.getElementById("appRoot")).render(<App />);
