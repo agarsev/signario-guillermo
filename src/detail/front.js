3e622a98184cb87b7650ee7d2da977b4567b87be
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2022-09-10 14:52:16 +0200

    Set video dir and load video

--- src/detail/front.js
+++ src/detail/front.js
@@ -1,7 +1,23 @@
 import { createRoot } from "react-dom/client";
 
+const urlParams = (new URL(document.location)).searchParams;
+const number = urlParams.get('number');
+const video_dir = urlParams.get('video_dir');
+
 function DetailFront () {
-    return <p>Hello Detail</p>;
+    return <>
+        <header>
+            Vídeo: {number}
+            <VideoPlay />
+        </header>
+    </>;
+}
+
+function VideoPlay () {
+    const video_src = `${video_dir}/${number.substring(0,3)}/${number.substring(3)}.mp4`;
+    return <video muted autoPlay>
+        <source src={video_src} />
+    </video>;
 }
 
 createRoot(document.getElementById("appRoot")).render(<DetailFront />);
--- src/main.js
+++ src/main.js
@@ -1,93 +1,96 @@
-const { app, BrowserWindow, Menu, ipcMain } = require('electron');
+const { app, BrowserWindow, Menu, dialog, ipcMain } = require('electron');
+const fs = require('fs');
 const path = require('path');
 
-function MainWindow (dir) {
-  Menu.setApplicationMenu(Menu.buildFromTemplate([{
-    role: 'fileMenu',
-    submenu: [{
-      type: 'separator'
-    }, {
-      role: 'quit'
-    }]
-  },{
-    role: 'editMenu',
-    submenu: [{
-      type: 'separator'
-    }, {
-      role: 'cut',
-    }, {
-      role: 'copy',
-    }, {
-      role: 'paste',
-    }]
-  },{
-    role: 'window',
-    submenu: [{
-      role: 'reload',
-    }, {
-      role: 'forceReload',
-    }, {
-      type: 'separator'
-    }, {
-      role: 'toggleDevTools',
-    }]
-  }]));
-  const win = new BrowserWindow({
+const prefs_path = path.join(app.getPath('userData'), "preferencias.json");
+let prefs = {}
+try {
+  prefs = JSON.parse(fs.readFileSync(prefs_path));
+} catch (err) {}
+prefs.set = (key, val) => {
+  prefs[key] = val;
+  fs.writeFileSync(prefs_path, JSON.stringify(prefs));
+}
+
+Menu.setApplicationMenu(Menu.buildFromTemplate([{
+  role: 'fileMenu',
+  submenu: [{
+    label: 'Carpeta de vídeos',
+    click: setVideoDir
+  }, {
+    type: 'separator'
+  }, {
+    role: 'quit'
+  }]
+},{
+  role: 'editMenu',
+  submenu: [{
+    type: 'separator'
+  }, {
+    role: 'cut',
+  }, {
+    role: 'copy',
+  }, {
+    role: 'paste',
+  }]
+},{
+  role: 'window',
+  submenu: [{
+    role: 'reload',
+  }, {
+    role: 'forceReload',
+  }, {
+    type: 'separator'
+  }, {
+    role: 'toggleDevTools',
+  }]
+}]));
+
+
+let main_window = null;
+app.whenReady().then(() => {
+  main_window = new BrowserWindow({
     webPreferences: {
       spellcheck: false,
       preload: path.join(__dirname, 'table/back.js'),
     },
   });
-  win.loadFile('dist/table/index.html');
-  win.on('closed', () => app.quit());
-}
+  main_window.loadFile('dist/table/index.html');
+  main_window.on('closed', () => app.quit());
+});
 
-app.whenReady()
-  .then(MainWindow);
-
-function DetailWindow () {
-    Menu.setApplicationMenu(Menu.buildFromTemplate([{
-        role: 'fileMenu',
-        submenu: [{
-            type: 'separator'
-        }, {
-            role: 'quit'
-        }]
-    },{
-        role: 'editMenu',
-        submenu: [{
-            type: 'separator'
-        }, {
-            role: 'cut',
-        }, {
-            role: 'copy',
-        }, {
-            role: 'paste',
-        }]
-    },{
-        role: 'window',
-        submenu: [{
-            role: 'reload',
-        }, {
-            role: 'forceReload',
-        }, {
-            type: 'separator'
-        }, {
-            role: 'toggleDevTools',
-        }]
-    }]));
-    return new BrowserWindow({
-        webPreferences: {
-            spellcheck: false,
-            preload: path.join(__dirname, 'detail/back.js'),
-        },
-    });
-}
 
 let detail_window = null;
-ipcMain.handle('open_detail', number => {
-    if (!detail_window) {
-        detail_window = DetailWindow();
+function loadDetail (number) {
+  if (number !== undefined) {
+    prefs.set('number', number);
+  }
+  detail_window.loadFile('dist/detail/index.html', {
+    query: {
+      number: prefs['number'],
+      video_dir: prefs['video_dir'],
     }
-    detail_window.loadFile('dist/detail/index.html');
+  });
+}
+ipcMain.handle('open_detail', (_, number) => {
+  if (!detail_window) {
+    detail_window = new BrowserWindow({
+      webPreferences: {
+        spellcheck: false,
+        preload: path.join(__dirname, 'detail/back.js'),
+      },
+    });
+  }
+  loadDetail(number);
 });
+
+async function setVideoDir (_, win) {
+  const res = await dialog.showOpenDialog(win, {
+    title: "Seleccionar carpeta de vídeos",
+    properties: ['openDirectory'],
+  });
+  if (!res.canceled) {
+    prefs.set('video_dir', res.filePaths[0]);
+    if (detail_window) loadDetail();
+  }
+}
